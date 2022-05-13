7bd7f88bd2a15779b85460f07f78c333ffe95195
zouhour-guiras <guiras.zouhour@gmail.com>
2022-05-13 08:07:46 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/gittest2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gittest2.dir/main.c.obj"
+  "gittest2.exe"
+  "gittest2.exe.manifest"
+  "gittest2.pdb"
+  "libgittest2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gittest2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
